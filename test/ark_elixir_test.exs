c76b5a4eff18191c1b1de18f6c8f732a87b16965
Highjhacker <mc-somango@live.fr>
2017-11-06 23:32:46 +0100

    Unit testing complete for the Loader.

--- test/ark_elixir_test.exs
+++ test/ark_elixir_test.exs
@@ -1,3 +1,23 @@
 defmodule Ark_ElixirTest do
-  use ExUnit.Case
+    use ExUnit.Case, async: true
+    import Ark_Elixir.Loader
+    #import Mock
+
+  test "get loader status" do
+      response = get_status()
+      success =  Enum.find(response, fn {key, _} -> key == "success" end)
+      assert elem(success, 1) == true
+  end
+
+  test "get loader sync" do
+      response = get_sync()
+      success =  Enum.find(response, fn {key, _} -> key == "success" end)
+      assert elem(success, 1) == true
+  end
+
+  test "get loader autoconfigure" do
+      response = autoconfigure()
+      success =  Enum.find(response, fn {key, _} -> key == "success" end)
+      assert elem(success, 1) == true
+  end
 end
