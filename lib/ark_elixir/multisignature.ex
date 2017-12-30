73ae821f45871e8fa85e5a9d5257d4fda9b1299e
sudomilk <mailertaylor@gmail.com>
2017-12-30 14:31:56 -0600

    added multisignature typespecs

--- lib/ark_elixir/multisignature.ex
+++ lib/ark_elixir/multisignature.ex
@@ -13,6 +13,7 @@ defmodule Ark_Elixir.MultiSignature do
 
         iex> Ark_Elixir.MultiSignature.get_pending("02c7455bebeadde04728441e0f57f82f972155c088252bf7c1365eb0dc84fbf5de", :dev)
     """
+    @spec get_pending(Account.public_key, Api.options) :: Api.response
     def get_pending(publicKey, opts \\ []) do
         Ark_Elixir.Api.get("api/multisignatures/pending", [publicKey: publicKey, network: opts])
     end
@@ -27,6 +28,7 @@ defmodule Ark_Elixir.MultiSignature do
         ...
         iex> Ark_Elixir.MultiSignature.get_accounts("validArkPublicKey", :dev)
     """
+    @spec get_accounts(Account.public_key, Api.options) :: Api.response
     def get_accounts(publicKey, opts \\ []) do
         Ark_Elixir.Api.get("api/multisignatures/accounts", [publicKey: publicKey, network: opts])
     end
